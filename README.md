098a02c8be0001dab0c64376c00b03fa3666eab3
EMILY GREASON <GREASONEMILY@GMAIL.COM>
2024-06-05 17:34:59 -0700

    Added about me page content with Lorem Ipsum, Grid and styling

--- README.md
+++ README.md
@@ -2,9 +2,25 @@
 
 ## Author: Emily Greason
 
+### Start Script
+
+- npm run dev
+
 ### Activity Log
+
+#### January 2024
 
 - created app with multiple pages for navigation
 - added a menu button with MaterialUI (non-navigational atm)
 - created some styling to home page
 - linked instagram page
+
+#### June 2 2024
+
+- Made home page and header responsive using Grid
+- Changed routing for contact button to about page
+- Added routing for shop button to go to Shop component
+
+#### June 3 2024
+
+- Added about me page content with Lorem Ipsum, Grid and styling
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,8 +1,21 @@
+import { Grid } from "@mui/material"
 const About = () => {
   return (
-    <>
-    about
-    </>
+    <Grid container spacing={3} justifyContent={'center'} alignItems={'center'} 
+      style={{
+        minHeight: '100vh', 
+        position: 'relative', 
+        zIndex: 1,
+        margin: 'auto'
+    }}>
+      <Grid item xs={12} sm={8} md={4} style={{ alignContent:'center'}}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce hendrerit felis at interdum maximus. Nunc vitae egestas diam. Nunc feugiat nibh quis lobortis ullamcorper. Praesent volutpat sit amet lorem eu tincidunt. Etiam nec molestie neque, ut finibus mi. Morbi laoreet accumsan blandit. Nunc semper risus vitae elementum malesuada. Integer nec ultricies felis. Nullam tristique, lectus in mollis vulputate, odio ipsum pellentesque libero, ac rutrum quam erat non diam. Curabitur hendrerit ligula vel aliquet interdum. In vitae gravida lorem. Nam at sapien vitae purus pulvinar tincidunt. Suspendisse sed aliquam mauris.
+       <br/>
+       <br/>
+        Duis iaculis eros fermentum luctus pretium. Mauris imperdiet lectus scelerisque, feugiat velit in, viverra massa. Pellentesque pellentesque posuere arcu. Aenean vel elementum neque. Ut id sapien rhoncus eros lobortis lobortis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam velit ante, lobortis vel pharetra sed, lacinia sit amet tortor. Sed convallis mi odio, in venenatis erat imperdiet sed. Nam et pulvinar ipsum. Vivamus hendrerit commodo massa, vitae consequat risus mattis tempor. Aenean nec orci vel velit dictum luctus sodales ut sem.
+      </Grid>
+
+    </Grid>
   )
 }
 
